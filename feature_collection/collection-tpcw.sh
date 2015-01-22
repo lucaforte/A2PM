d943c81fc8eb8b8e6a312813f9e2c151e9b21d8c
Alessandro Pellegrini <pellegrini@dis.uniroma1.it>
2015-01-22 07:46:21 -0800

    Updating collection script

--- feature_collection/collection-tpcw.sh
+++ feature_collection/collection-tpcw.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-
-repeat=$(cat /home/user/collection/repeat)
-
-if [ "$repeat" -eq "1" ]; then
-	echo "Lo script si disabiliterà ora..."
-	exit 0;
-else
-	echo "Avvio il ciclo di trasmissione"
-fi
-
-let repeat=$repeat+1
-echo $repeat > /home/user/collection/repeat
-
-
-sleep 20;
-
 while true;
 do
-	unbuffer ./feature_client 192.168.199.1 | (./halter 8 600000 ; killall feature_client)
+	unbuffer ./lient 192.168.112.129 | (./halter 8 600000 ; killall client)
 	/etc/init.d/tomcat6 restart
-#	sudo reboot
 done
