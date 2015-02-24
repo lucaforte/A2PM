dd89789e1c395caa671c65fa492d7546b16aa42f
Alessandro Pellegrini <pellegrini@dis.uniroma1.it>
2015-02-24 14:49:42 +0000

    Using proc to estimate CPU usage

--- feature_collection/client.c
+++ feature_collection/client.c
@@ -7,10 +7,13 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <unistd.h>
 
 
 #define BUFSIZE 4096
 
+
+int num_cpus;
 
 char send_buff[BUFSIZE];
 
@@ -22,22 +25,31 @@ void get_features(char *output){
 	int line; char ch;
 	int mem_total, mem_used, mem_free, mem_shared, mem_buffers, mem_cached;
 	int swap_total, swap_used, swap_free;
-	float cpu_user,cpu_nice, cpu_system, cpu_iowait, cpu_steal, cpu_idle;
+	unsigned long cpu_user1, cpu_nice1, cpu_system1, cpu_iowait1, cpu_steal1, cpu_idle1, dummyA1, dummyB1, dummyC1, dummyD1;
+	unsigned long cpu_user2, cpu_nice2, cpu_system2, cpu_iowait2, cpu_steal2, cpu_idle2, dummyA2, dummyB2, dummyC2, dummyD2;
+	float cpu_user, cpu_nice, cpu_system, cpu_iowait, cpu_steal, cpu_idle;
+	float cpu_total;
 	struct timeval curr_time;
 
 	char num_th[128];
-	FILE *pof;
+	FILE *pof, *fstat;
 
+	/* Get number of active threads */
 	pof = popen("ps -eLf | grep -v defunct | wc -l", "r");
 	if(pof == NULL)
 		abort();
 
-	fgets(num_th, sizeof(num_th)-1, pof);	
+	fgets(num_th, sizeof(num_th)-1, pof);
+	pclose(pof);
 	
 
+	/* Get timestamp */
 	gettimeofday(&curr_time, NULL);
 	sprintf(output, "Datapoint: %f %s", (double)curr_time.tv_sec - initial_time.tv_sec + (double)curr_time.tv_usec / 1000000 - (double)initial_time.tv_usec / 1000000, num_th);
 
+
+
+	/* Get memory and swap usage */
 	command = "free > memdata.txt";
 	system(command);
 	t = fopen("memdata.txt","r");
@@ -61,24 +73,41 @@ void get_features(char *output){
 	sprintf(output,"%sSwap: %d %d %d\n",output,swap_total,swap_used,swap_free);	
 	fclose(t);
 	
-	// Replaced the old iostat with top to get accurate CPU usage
-	//command = "iostat > cpudata.txt";
-	command = "top -d 0.2 -b -n2 | grep \"Cpu(s)\" | tail -n 1 | sed 's/%/ /g' > cpudata.txt";
-	system(command);
-	t = fopen("cpudata.txt","r");
-/*	for(line = 1;line <= 1;line++){
-		int dont_stop = 1;
-		while(dont_stop){
-			fscanf(t,"%c",&ch);
-			if(ch=='\n')dont_stop = 0;
-		}
+
+	/* Get CPU Usage (using /proc/stat) */
+	fstat = fopen("/proc/stat", "r");
+	if (fstat == NULL) {
+		perror("FOPEN ERROR ");
+		exit(EXIT_FAILURE);
 	}
-	printf("%s\n", t);
-*/	fscanf(t,"Cpu(s): %f us, %f sy, %f ni, %f id, %f wa, %f hi, %f si, %f st",&cpu_user,&cpu_system,&cpu_nice,&cpu_idle, &cpu_iowait,&cpu_steal,&cpu_steal,&cpu_steal);
-	sprintf(output,"%sCPU: %f %f %f %f %f %f",output,cpu_user,cpu_nice,cpu_system,cpu_iowait,cpu_steal,cpu_idle);
+	if (fscanf(fstat, "%*s %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu", &cpu_user1, &cpu_nice1, &cpu_system1, &cpu_idle1, &cpu_iowait1, &dummyA1, &dummyB1, &cpu_steal1, &dummyC1, &dummyD1) == EOF) {
+		exit(EXIT_FAILURE);
+	}
+	fclose(fstat);
+	sleep(1);
+	fstat = fopen("/proc/stat", "r");
+        if (fstat == NULL) {
+                perror("FOPEN ERROR ");
+                exit(EXIT_FAILURE);
+        }
+        if (fscanf(fstat, "%*s %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu", &cpu_user2, &cpu_nice2, &cpu_system2, &cpu_idle2, &cpu_iowait2, &dummyA2, &dummyB2, &cpu_steal2, &dummyC2, &dummyD2) == EOF) {
+                exit(EXIT_FAILURE);
+        }
+        fclose(fstat);
+
+	cpu_total = (float)(cpu_user2 + cpu_nice2 + cpu_system2 + cpu_idle2 + cpu_iowait2 + dummyA2 + dummyB2 + cpu_steal2 + dummyC2 + dummyD2);
+	cpu_total -= (float)(cpu_user1 + cpu_nice1 + cpu_system1 + cpu_idle1 + cpu_iowait1 + dummyA1 + dummyB1 + cpu_steal1 + dummyC1 + dummyD1);
+
+	cpu_user = (float)(cpu_user2 - cpu_user1) * 100.0 / cpu_total;
+	cpu_system = (float)(cpu_system2 - cpu_system1) * 100.0 / cpu_total;
+	cpu_nice = (float)(cpu_nice2 - cpu_nice1) * 100.0 / cpu_total;
+	cpu_idle = (float)(cpu_idle2 - cpu_idle1) * 100.0 / cpu_total;
+	cpu_iowait = (float)(cpu_iowait2 - cpu_iowait1) * 100.0 / cpu_total;
+	cpu_steal = (float)(cpu_steal2 - cpu_steal1) * 100.0 / cpu_total;
+
+	sprintf(output,"%sCPU: %f %f %f %f %f %f", output, cpu_user, cpu_nice, cpu_system, cpu_iowait, cpu_steal, cpu_idle);
 
 	printf("%s\n",output);
-	fclose(t);
 	
 	return;
 }
@@ -115,6 +144,8 @@ int main(int argc, char **argv) {
 	int numbytes;
 	connect_to_server(&sockfd, argv[1]);
 	gettimeofday(&initial_time, NULL);
+
+	num_cpus = sysconf(_SC_NPROCESSORS_ONLN);
 	
 	while(true) {	
 		bzero(send_buff, BUFSIZE);
